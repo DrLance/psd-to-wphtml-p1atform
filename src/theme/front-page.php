f63dfafca578bdf48862e3e121b4f3ba53fcb311
Peter Klimenko <megalance@gmail.com>
2018-11-08 18:23:42 +0300

    Continue...

--- src/theme/front-page.php
+++ src/theme/front-page.php
@@ -3,22 +3,22 @@
 
 <div class="menu-container">
     <div class="container">
-		<?php wp_nav_menu( array(
-			'theme_location' => '',
-			'menu'           => '',
-			'container'      => false,
-			'container_id'   => '',
-			'menu_class'     => 'row',
-			'menu_id'        => '',
-			'echo'           => true,
-			'fallback_cb'    => 'wp_page_menu',
-			'after'          => '<span>|</span>',
-			'link_before'    => '',
-			'link_after'     => '',
-			'items_wrap'     => '<ul id="%1$s">%3$s</ul>',
-			'depth'          => 0,
-			'walker'         => '',
-		) ); ?>
+			<?php wp_nav_menu( array(
+				'theme_location' => '',
+				'menu'           => '',
+				'container'      => false,
+				'container_id'   => '',
+				'menu_class'     => 'row',
+				'menu_id'        => '',
+				'echo'           => true,
+				'fallback_cb'    => 'wp_page_menu',
+				'after'          => '<span>|</span>',
+				'link_before'    => '',
+				'link_after'     => '',
+				'items_wrap'     => '<ul id="%1$s">%3$s</ul>',
+				'depth'          => 0,
+				'walker'         => '',
+			) ); ?>
     </div>
 </div>
 
@@ -53,46 +53,46 @@
 
 </div>
 
-<section class="container">
+<section class="container p-0">
 
     <h2 class="header-text text-center">КОМПАНИЯ НА РЫНКЕ УЖЕ БОЛЕЕ 15 ЛЕТ</h2>
 
     <h3 class="text-center page-header-text">НАШИ ПРЕИМУЩЕСТВА</h3>
-    <div class="d-flex align-items-end justify-content-end mt-lg-5 container-text">
-        <div class="col-md-3 text-center">
-            <img class="mb-2" src="<?php echo get_template_directory_uri(); ?>/img/top.png">
+    <div class="d-flex align-items-baseline justify-content-between container-text">
+        <div class="text-center">
+            <img class="mb-auto p-2" src="<?php echo get_template_directory_uri(); ?>/img/top.png">
             <br/>
             <span>точно выведем в ТОП</span>
         </div>
-        <div class="col-md-3 text-center">
-            <img class="mb-2 mt-3" src="<?php echo get_template_directory_uri(); ?>/img/experience.png">
+        <div class="text-center">
+            <img class="mb-auto p-2" src="<?php echo get_template_directory_uri(); ?>/img/experience.png">
             <br/>
             <span>опыт и компетенция</span>
         </div>
-        <div class="col-md-3 text-center">
-            <img class="mb-md-4" src="<?php echo get_template_directory_uri(); ?>/img/good_price.png">
+        <div class="text-center">
+            <img class="mb-auto p-2" src="<?php echo get_template_directory_uri(); ?>/img/good_price.png">
             <br/>
             <span>хорошие цены</span>
         </div>
-        <div class="col-md-3 text-center">
-            <img class="mb-1" src="<?php echo get_template_directory_uri(); ?>/img/reliability.png">
+        <div class="text-center">
+            <img class="mb-auto p-2" src="<?php echo get_template_directory_uri(); ?>/img/reliability.png">
             <br/>
             <span>надежность</span>
         </div>
     </div>
 
-    <div class="row d-flex align-items-baseline container-text mt-5">
-        <div class="col-md-4 text-center">
+    <div class="row d-flex align-items-baseline justify-content-around container-text mt-5">
+        <div class="text-center">
             <img class="mb-3" src="<?php echo get_template_directory_uri(); ?>/img/anyone_form_paid.png">
             <br/>
             <span>любые формы оплаты</span>
         </div>
-        <div class="col-md-4 text-center">
+        <div class="text-center">
             <img class="mb-3 mt-3" src="<?php echo get_template_directory_uri(); ?>/img/reporting.png">
             <br/>
             <span>отчетность</span>
         </div>
-        <div class="col-md-4 text-center">
+        <div class="text-center">
             <img class="mb-3" src="<?php echo get_template_directory_uri(); ?>/img/goodwill.png">
             <br/>
             <span>доброжелательность</span>
@@ -101,43 +101,43 @@
 
 </section>
 
-<section class="container section-develop">
-    <div class="d-flex justify-content-between">
-        <div class="background-block-develop">
+<section class="container section-develop p-0">
+    <div class="d-flex benefits-container">
+        <div class="develop">
             <div class="col-md-6 p-0 site">
                 <span>разработка сайтов и сервисов</span>
             </div>
             <img src="<?php echo get_template_directory_uri(); ?>/img/develop_site.png">
         </div>
-        <div class="background-block-develop">
+        <div class="context-adv">
             <div class="col-md-3 p-0 site">
                 <span>реклама в интернете</span>
             </div>
             <img src="<?php echo get_template_directory_uri(); ?>/img/adv_internet.png">
         </div>
     </div>
-    <div class="d-flex justify-content-between mt-4">
-        <div class="d-flex flex-column">
-            <div class="background-block-audit">
-                <div class="col-md-6 p-0 site">
+    <div class="d-flex mt-4 benefits-container">
+        <div class="d-flex flex-column ">
+            <div class="audit">
+                <div class="col-md-4 p-0 site">
                     <span>аудиты сайта</span>
                 </div>
                 <img src="<?php echo get_template_directory_uri(); ?>/img/audit.png">
             </div>
-            <div class="background-block-seo">
+            <div class="seo">
                 <div class="col-md-3 p-0 site">
                     <span>продвижение сайта</span>
                 </div>
                 <img src="<?php echo get_template_directory_uri(); ?>/img/seo.png">
             </div>
         </div>
-        <div class="background-block-support">
+        <div class="support">
             <div class="col-md-6 p-0 site">
                 <span>техническая поддержка</span>
             </div>
             <img src="<?php echo get_template_directory_uri(); ?>/img/support.png">
         </div>
-        <div class="background-block-adv">
+        <div class="adv">
             <div class="col-md-3 p-0 site">
                 <span>контекстная реклама</span>
             </div>
@@ -149,9 +149,173 @@
 <section>
     <h3 class="text-center page-header-text">НАШИ УСЛУГИ</h3>
 
-    <div class="d-flex w-100 container-service">
+    <div class="d-flex w-100 container-service flex-column">
+        <div class="container p-0">
+            <div class="d-flex">
+                <div class="ourservice-container d-flex flex-column align-items-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/develop_site_service.png">
+                    <h4>создание сайтов</h4>
 
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Проекты любой сложности</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Интеграция стороних сервисов</a>
+                    </div>
+                    <div class="w-75 mb-auto w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Современная система управления</a>
+                    </div>
+
+                    <div class="align-self-end footer-service d-flex align-items-center justify-content-center">
+                        <span>От 50 000 руб.</span>
+                    </div>
+
+                </div>
+                <div class="ourservice-container d-flex flex-column align-items-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/seo_service.png">
+                    <h4>продвижение сайтов</h4>
+
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Различные схемы продвижения</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Комплексный подход к проекту</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Оптимальные сроки достижения результата</a>
+                    </div>
+                    <div class="w-75 mb-auto  w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Прозрачность всех работ</a>
+                    </div>
+
+
+                    <div class="footer-service d-flex align-items-center justify-content-center">
+                        <span>От 15 000 руб.</span>
+                    </div>
+                </div>
+                <div class="ourservice-container d-flex flex-column align-items-center mr-0">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/seo_social_service.png">
+                    <h4>продвижение в соцсетях</h4>
+
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Охват всех популярных сетей</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Поддержка внимания к бренду</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Улучшения лояльности клиентов</a>
+                    </div>
+                    <div class="w-75 w-block mb-auto">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Привлечения трафика на сайт</a>
+                    </div>
+
+
+                    <div class="footer-service d-flex align-items-center justify-content-center">
+                        <span>От 15 000 руб.</span>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="d-flex">
+                <div class="ourservice-container d-flex flex-column align-items-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/adv_internet_service.png">
+                    <h4>реклама в интернете</h4>
+
+                    <div class="w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Яндекс.Директ</a>
+                    </div>
+                    <div class="w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Google Adwords</a>
+                    </div>
+                    <div class="w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Яндекс.Маркет</a>
+                    </div>
+                    <div class="w-block mb-auto">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Медийная (банерная) реклама</a>
+                    </div>
+
+                    <div class="footer-service d-flex align-items-center justify-content-center">
+                        <span>От 5 000 руб.</span>
+                    </div>
+                </div>
+                <div class="ourservice-container d-flex flex-column align-items-center">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/firm_style_service.png">
+                    <h4>фирменный стиль</h4>
+
+                    <div class="w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Логотип</a>
+                    </div>
+                    <div class="w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Фирменный бланк</a>
+                    </div>
+                    <div class="w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Конверт</a>
+                    </div>
+
+                    <div class="w-block mb-auto">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Визитка</a>
+                    </div>
+
+                    <div class="footer-service d-flex align-items-center justify-content-center">
+                        <span>От 10 000 руб.</span>
+                    </div>
+                </div>
+                <div class="ourservice-container d-flex flex-column align-items-center mr-0">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/support_site_service.png">
+                    <h4>поддержка сайтов</h4>
+
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Хостинг</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Системной администрирование</a>
+                    </div>
+                    <div class="w-75 w-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Контент-менеджмент</a>
+                    </div>
+                    <div class="w-75 w-block mb-auto">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_down.png">
+                        <a href="#">Копирайтинг</a>
+                    </div>
+                    <div class="footer-service d-flex align-items-center justify-content-center">
+                        <span>От 2 000 руб.</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <button class="align-self-end align-self-center button mt-auto mb-auto">оставить заявку</button>
     </div>
+</section>
+<section class="d-flex flex-column cases-container">
+
+    <h3 class="align-self-center page-header-text">КЕЙСЫ</h3>
+
+
 </section>
 
 
